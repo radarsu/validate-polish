c3179967e433ee2cf8c5cc72ea5f2a00a6086c34
radarsu <rkrohmorath@gmail.com>
2016-05-22 10:21:52 +0200

    little fix

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ polishValidator.prototype = {
         if (reg.test(pesel) === false) {
             return false;
         } else {
+			
             var dig = ("" + pesel).split("");
             var control = (1 * parseInt(dig[0]) + 3 * parseInt(dig[1]) + 7 * parseInt(dig[2]) + 9 * parseInt(dig[3]) + 1 * parseInt(dig[4]) + 3 * parseInt(dig[5]) + 7 * parseInt(dig[6]) + 9 * parseInt(dig[7]) + 1 * parseInt(dig[8]) + 3 * parseInt(dig[9])) % 10;
             if (control === 0) {
@@ -19,6 +20,7 @@ polishValidator.prototype = {
             } else {
                 return false;
             }
+			
         }
     },
     nip: function(nip) {
@@ -27,6 +29,7 @@ polishValidator.prototype = {
         if (reg.test(nipWithoutDashes) === false) {
             return false;
         } else {
+			
             var dig = ("" + nipWithoutDashes).split("");
             var control = (6 * parseInt(dig[0]) + 5 * parseInt(dig[1]) + 7 * parseInt(dig[2]) + 2 * parseInt(dig[3]) + 3 * parseInt(dig[4]) + 4 * parseInt(dig[5]) + 5 * parseInt(dig[6]) + 6 * parseInt(dig[7]) + 7 * parseInt(dig[8])) % 11;
             if (parseInt(dig[9]) === control) {
@@ -34,6 +37,7 @@ polishValidator.prototype = {
             } else {
                 return false;
             }
+			
         }
     },
     regon: function(regon) {
@@ -41,6 +45,7 @@ polishValidator.prototype = {
         if (reg.test(regon) === false) {
             return false;
         } else {
+			
             var dig = ("" + regon).split("");
             var control = (8 * parseInt(dig[0]) + 9 * parseInt(dig[1]) + 2 * parseInt(dig[2]) + 3 * parseInt(dig[3]) + 4 * parseInt(dig[4]) + 5 * parseInt(dig[5]) + 6 * parseInt(dig[6]) + 7 * parseInt(dig[7])) % 11;
             if (control == 10) control = 0;
@@ -49,16 +54,17 @@ polishValidator.prototype = {
             } else {
                 return false;
             }
+			
         }
     },
     identityCard: function(number) {
         // Check length
-        if (number === null || number.length !== 9) {
+        if (!number || number.length !== 9) {
             return false;
         }
 
         number = number.toUpperCase();
-        letterValues = [
+        var letterValues = [
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
             'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
@@ -67,7 +73,7 @@ polishValidator.prototype = {
 
         var getLetterValue = function(letter) {
             for (j = 0; j < letterValues.length; j++) {
-                if (letter == letterValues[j]) {
+                if (letter === letterValues[j]) {
                     return j;
                 }
                 return -1;
@@ -88,7 +94,7 @@ polishValidator.prototype = {
         }
 
         // Checksum
-        sum = 7 * getLetterValue(number[0]) +
+        var sum = 7 * getLetterValue(number[0]) +
             3 * getLetterValue(number[1]) +
             1 * getLetterValue(number[2]) +
             7 * getLetterValue(number[4]) +
@@ -96,8 +102,10 @@ polishValidator.prototype = {
             1 * getLetterValue(number[6]) +
             7 * getLetterValue(number[7]) +
             3 * getLetterValue(number[8]);
-        sum %= 10;
-        if (sum != getLetterValue(number[3])) {
+        
+		sum %= 10;
+	
+        if (sum !== getLetterValue(number[3])) {
             return false;
         }
 
@@ -110,14 +118,17 @@ polishValidator.prototype = {
      */
     identityCardWithSeparator: function(number) {
         var self = this;
+
         //Check length
-        if (number === null || number.length !== 10) {
+        if (!number || number.length !== 10) {
             return false;
         }
+
         //Check separator
         if (number[3] !== ' ' && number[3] !== '-') {
             return false;
         }
+
         number = number.replace(/[\s-]/g, '');
         return self.identityCard(number);
     }
